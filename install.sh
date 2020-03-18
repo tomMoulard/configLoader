e2b42fe69a0b463c3981cfdad4e79a5d0ca29f2d
Tom Moulard <tom.moulard@epita.fr>
2020-03-18 08:57:20 +0100

    install: fixing test for files and directories

--- install.sh
+++ install.sh
@@ -5,59 +5,64 @@
 
 # createLink create a link between the file in the git repo and the output file
 # $1 must be the file path
+# $2 must be the test $(man test)
 # $2 must be the output file path
 function createLink() {
-    if [ -L "$2" ]; then
-        rm -r "$2"
+    if [ -L "$3" ]; then
+        rm -r "$3"
     fi
-    if [ -f "$2" ]; then
-        mv "$2" "${2}.old"
+    if [ ${2} "$3" ]; then
+        mv "$3" "${3}.$(date +"%y%m%d%H%M%S").old"
     fi
-    ln -s "$PWD/${1}" "$2"
+    ln -s "$PWD/${1}" "$3"
 }
 
 # Background picture
 wget -q https://tom.moulard.org/picts/background.jpg -O background/background.jpg
 
 # Profile
-createLink profile "$HOME/.profile"
+createLink profile -f "$HOME/.profile"
 
 # Xressourses
-createLink Xresources "$HOME/.Xresources"
-[[ -f /usr/bin/xrdb ]] && "xrdb $HOME/.Xresources"
+createLink Xresources -f "$HOME/.Xresources"
+[[ -f /usr/bin/xrdb ]] && bash -c "/usr/bin/xrdb $HOME/.Xresources"
 
 # Font
-createLink fonts "$HOME/.fonts"
+createLink fonts -d "$HOME/.fonts"
 
 # Vim
-createLink vimrc "$HOME/.vimrc"
-createLink vim "$HOME/.vim"
+createLink vimrc -f "$HOME/.vimrc"
+createLink vim -d "$HOME/.vim"
 
 # SSH
-createLink ssh "$HOME/.ssh"
+createLink ssh -d "$HOME/.ssh"
 
 # Bash
-createLink bashrc "$HOME/.bashrc"
-createLink aliases "$HOME/.bash_aliases"
-createLink bash_functions "$HOME/.bash_functions"
+createLink bashrc -f "$HOME/.bashrc"
+createLink aliases -f "$HOME/.bash_aliases"
+createLink bash_functions -f "$HOME/.bash_functions"
 
 # Rxvt
-createLink urxvt "$HOME/.urxvt"
+createLink urxvt -d "$HOME/.urxvt"
 
 # xinitrc
-createLink xinitrc "$HOME/.xinitrc"
+createLink xinitrc -f "$HOME/.xinitrc"
 
 # gdbinit
-createLink gdbinit "$HOME/.gdbinit"
+createLink gdbinit -f "$HOME/.gdbinit"
 
 # xmodmap
-createLink Xmodmap "$HOME/.Xmodmap"
+createLink Xmodmap -f "$HOME/.Xmodmap"
 
 # dig
-createLink digrc "$HOME/.digrc"
+createLink digrc -f "$HOME/.digrc"
+
+# gesture
+mkdir -p "$HOME/.config/fusuma/"
+createLink fusuma.yml -f "$HOME/.config/fusuma/config.yml"
 
 # config
 mkdir -p "$HOME/.config/"
 for FOLDER in config/*; do
-    createLink "$FOLDER" "$HOME/.$FOLDER"
+    createLink "$FOLDER" -d "$HOME/.$FOLDER"
 done
