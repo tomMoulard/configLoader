c60ac84d0e39a14d5801c8b4b0753334e5ea6fc1
Tom Moulard <tom.moulard@traefik.io>
2021-04-22 14:16:02 +0200

    autoUpdatePlugins.sh: fmt

--- vim/autoUpdatePlugins.sh
+++ vim/autoUpdatePlugins.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-for x in bundle/*; do echo $x; cd $x; git pull origin master; cd ../..; done
+for x in bundle/*; do
+	echo $x
+	cd $x
+	git pull origin master
+	cd ../..
+done
 
 # if busy:
 # git submodule foreach git pull origin master
