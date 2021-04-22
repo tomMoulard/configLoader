2ff12a2cef58b61878859f55391e3ccf3df3f587
Tom Moulard <tom.moulard@traefik.io>
2021-04-22 08:57:38 +0200

    vim: updating plugins

--- vim/autoUpdatePlugins.sh
+++ vim/autoUpdatePlugins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for x in bundle/*; do echo $x; cd $x; git pull; cd ../..; done
+for x in bundle/*; do echo $x; cd $x; git pull origin master; cd ../..; done
 
 # if busy:
 # git submodule foreach git pull origin master
