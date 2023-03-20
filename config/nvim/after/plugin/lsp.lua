2fd2180025e8ce4058cd35aae69541300d4cb17e
Tom Moulard <tom.moulard@traefik.io>
2023-03-20 11:12:14 +0100

    vim: do not check third party for lua lsp

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -135,6 +135,8 @@ if (vim.fn.executable("lua-language-server") == 1) then
 				workspace = {
 					-- Make the server aware of Neovim runtime files
 					library = vim.api.nvim_get_runtime_file("", true),
+					-- https://github.com/neovim/nvim-lspconfig/issues/1700
+					checkThirdParty = false,
 				},
 				telemetry = {
 					-- Do not send telemetry data
