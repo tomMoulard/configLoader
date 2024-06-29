0e5b24632df84dff81e94a217c2ca98a30a5d17b
Tom Moulard <tom@moulard.org>
2024-06-29 16:51:46 +0200

    feat: add workspace folders to nvim copilot

--- config/nvim/after/plugin/copilot.lua
+++ config/nvim/after/plugin/copilot.lua
@@ -4,4 +4,10 @@ vim.g.copilot_filetypes = {
 	["markdown"] = true,
 }
 
+vim.g.copilot_workspace_folders = {
+	"~/go/src/github.com/traefik/",
+	"~/Documents/backup/2023-04-25-traefik/traefik/",
+	"~/go/src/github.com/tomMoulard/",
+}
+
 vim.keymap.set("i", "<C-j>", vim.fn["copilot#Accept"], { silent = true, expr = true, script = true, replace_keycodes = false })
